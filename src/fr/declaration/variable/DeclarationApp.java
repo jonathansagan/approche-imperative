92b38896e85040fa2ba7131afd3ff251300d9906
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-02-28 12:19:24 +0100

    ok tp 1

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,36 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 3;
+		short b = 4;
+		int i = 589;
+		long zeta = 4587944455511l;
+		float beta = 1.8f;
+		double dx = 65.8d;
+		char lettre= 'l';
+		boolean q = true;
+		
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(i);
+		System.out.println(zeta);
+		System.out.println(beta);
+		System.out.println(dx);
+		System.out.println(lettre);
+		System.out.println(q);
+		
+		String randomString = "Voici le résultat d’un calcul : 1+5=6";
+		
+		System.out.println(randomString);
+		
+String randomString2 = "Voici le résultat d’un calcul :\n1+5=6";
+		
+		System.out.println(randomString2);
+
+	}
+
+}
