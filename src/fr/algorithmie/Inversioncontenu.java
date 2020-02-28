ad3da137073ba1b2a5ce9141db8e56e18509a109
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-02-28 17:33:35 +0100

    en cours

--- src/fr/algorithmie/Inversioncontenu.java
+++ src/fr/algorithmie/Inversioncontenu.java
@@ -1,10 +1,17 @@
+//exercice 2
 package fr.algorithmie;
 
 public class Inversioncontenu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//q1
+		int[] array = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
+		int [] arrayCopy = new int [array.length];
+		for (int i = array.length-1;i>=0;i--);
+		int j = array.length-1-i;
+		arrayCopy[j]=array[i];
+		
+		
 	}
 
 }
