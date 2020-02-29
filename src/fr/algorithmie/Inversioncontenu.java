3d5599b50f674574c62a328cd8df1bde98e5ca40
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-02-29 15:55:34 +0100

    Exercice 2

--- src/fr/algorithmie/Inversioncontenu.java
+++ src/fr/algorithmie/Inversioncontenu.java
@@ -2,16 +2,30 @@
 package fr.algorithmie;
 
 public class Inversioncontenu {
-
 	public static void main(String[] args) {
-//q1
+//Soit le tableau suivant :
 		int[] array = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
-		int [] arrayCopy = new int [array.length];
-		for (int i = array.length-1;i>=0;i--);
-		int j = array.length-1-i;
-		arrayCopy[j]=array[i];
-		
-		
-	}
 
+		// Créer un tableau arrayCopy et copier tous les éléments de array dans
+		// arrayCopy mais dans l’ordre inverse.
+		int[] arrayCopy = new int[array.length];
+
+		int j = array.length - 1;
+		for (int i = 0; i < array.length; i++) {
+
+			arrayCopy[j] = array[i];
+			j--;
+		}
+
+		// Afficher l’ensemble des éléments des 2 tableaux
+		System.out.println("array :");
+		for (int nombres : array) {
+			System.out.println(nombres);
+		}
+
+		System.out.println("arrayCopy :");
+		for (int nombres : arrayCopy) {
+			System.out.println(nombres);
+		}
+	}
 }
